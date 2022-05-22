f1b782e63ef8470e6ee5ab4604e332aa3bfe6009
Giovanni Dejan <giovanni.dejan@traveloka.com>
2022-05-22 17:54:58 +0700

    Add secondary IP ranges

--- 03-network.tf
+++ 03-network.tf
@@ -10,4 +10,14 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
+  secondary_ip_range = [
+    {
+      ip_cidr_range = "10.244.0.0/14"
+      range_name    = "gke-${var.project_id}-gke-pods-72af974f"
+    },
+    {
+      ip_cidr_range = "10.248.0.0/20"
+      range_name    = "gke-${var.project_id}-gke-services-72af974f"
+    },
+  ]
 }
